d9495699553e3ec6e5a93aa63528a654b24f5f8c
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2023-06-14 16:52:35 +0700

    00:29:00

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * PRELOAD
+ * 
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function(){
+    preloader.classList.add("loaded");
+    this.document.body.classList.add("loaded");
+});
