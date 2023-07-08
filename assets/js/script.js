bc5b9c507fc0eb9d6b61dd69708139b20d3aa6a7
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2023-07-08 17:46:09 +0700

    01:38:00

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,3 +64,64 @@ window.addEventListener("scroll", function(){
         header.classList.remove("active");
     }
 });
+
+/**
+ * HERO SLIDER
+ */
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSlidePos = function(){
+    lastActiveSliderItem.classList.remove("active");
+    heroSliderItems[currentSlidePos].classList.add("active");
+    lastActiveSliderItem = heroSliderItems[currentSlidePos];
+}
+
+const slideNext = function(){
+    if (currentSlidePos >= heroSliderItems.length - 1){
+        currentSlidePos = 0;
+    }else{
+        currentSlidePos++;
+    }
+
+    updateSlidePos();
+}
+
+heroSliderNextBtn.addEventListener("click", slideNext);
+
+const slidePrev = function(){
+    if(currentSlidePos <= 0){
+        currentSlidePos = heroSliderItems.length -1;
+    }else{
+        currentSlidePos--;
+    }
+
+    updateSlidePos();
+}
+
+heroSliderPrevBtn.addEventListener("click", slidePrev);
+
+
+/**
+ * auto slide
+ */
+let autoSlideInterval;
+
+const autoSlide = function (){
+    autoSlideInterval = setInterval( function(){
+        slideNext();
+    }, 5000);
+} 
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function(){
+    clearInterval(autoSlideInterval);
+});
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout", autoSlide);
+
+window.addEventListener("load", autoSlide);
